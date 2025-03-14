bb1eb353c005aa419746ffd0b9ba76f24e82414d
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2025-03-13 21:53:48 -0300

    animação

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,14 @@
 import { StyledHome } from "./styled";
 
 import foto1 from '../../assets/1.png'
+import foto2 from '../../assets/2.png'
 import foto3 from '../../assets/3.png'
+import foto4 from '../../assets/4.png'
+import foto5 from '../../assets/5.png'
+import foto6 from '../../assets/6.png'
+import foto7 from '../../assets/7.png'
+
+import foto9 from '../../assets/9.png'
 
 
 export default function Home() {
@@ -15,10 +22,14 @@ export default function Home() {
 
             <section>
                 <img id="img1" src={foto1} alt="img" />
-                <img id="img2" src={foto1} alt="img" />
+                <img id="img2" src={foto2} alt="img" />
                 <img id="img3" src={foto3} alt="img" />
-                <img id="img4" src={foto1} alt="img" />
-               
+                <img id="img4" src={foto4} alt="img" />
+                <img id="img5" src={foto5} alt="img" />
+                <img id="img6" src={foto6} alt="img" />
+                <img id="img7" src={foto7} alt="img" />
+                
+                <img id="img9" src={foto9} alt="img" />
             </section>
 
 
--- src/Pages/Home/styled.js
+++ src/Pages/Home/styled.js
@@ -24,37 +24,65 @@ export const StyledHome = styled.main`
     section{
         width: 100%;
         height: 100px;
-        border: solid 2px red;
-
-
+       
         display: flex;
         align-items: center;
         justify-content: center;
     }
     #img1{
         position: absolute;
-        top: 25%;
-        left: 15%;
+        top: 45%;
+        left: 5%;
         width: 17%;
-        z-index: 2;
+        z-index: 9;
     }
     #img2{
         position: absolute;
-        top: 30%;
-        left: 30%;
+        top: 25%;
+        left: 20%;
         width: 16%;
-        z-index: 1;
+        z-index: 8;
     }
 
     #img3{
         position: absolute;
-        top: 35%;
-        left: 45%;
+        top: 50%;
+        left: 35%;
         width: 20%;
-        z-index: 2;
+        z-index: 9;
     }
 
     #img4{
+        position: absolute;
+        top: 30%;
+        left: 33%;
+        width: 20%;
+        z-index: 6;
+    }
+
+    #img5{
+        position: absolute;
+        top: 30%;
+        left: 45%;
+        width: 20%;
+       
+        z-index: 7;
+    }
+    #img6{
+        position: absolute;
+        top: 45%;
+        left: 55%;
+        width: 17%;
+        z-index: 6;
+    }
+    #img7{
+        position: absolute;
+        top: 30%;
+        left: 70%;
+        width: 17%;
+        z-index: 5;
+    }
+    #img8{
         position: absolute;
         top: 45%;
         right: 10%;
@@ -62,18 +90,20 @@ export const StyledHome = styled.main`
         height: 250px;
         z-index: 1;
     }
-    
+    #img9{
+        position: absolute;
+        top: 20%;
+        right: 2%;
+        width: 17%;
+        z-index: 6;
+    }
 
     #rodape p{
         font-size: 2rem;
+        cursor: pointer;
     }
 
  
-
-
-
-
-
 
     #rodape{
         width: 90%;
@@ -83,6 +113,120 @@ export const StyledHome = styled.main`
         display: flex;
         align-items: center;
         justify-content: space-between;
+    }
+
+
+     /* ANIMAÇÕES */
+     @keyframes fromLeft {
+        from {
+            transform: translateX(-100vw);
+            opacity: 0;
+        }
+        to {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes fromRight {
+        from {
+            transform: translateX(100vw);
+            opacity: 0;
+        }
+        to {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+
+    /* Aplicando animações */
+    #img1, #img3, #img5, #img7, #img9 {
+        animation: fromLeft 2s ease-out forwards;
+    }
+
+    #img2, #img4, #img6, #img8 {
+        animation: fromRight 2s ease-out forwards;
+    }
+
+
+
+
+    @media (max-width: 800px) {
+        h1{
+        position: fixed;
+        top: 50px;
+        font-size:5rem;
+        z-index: 0;
+    }
+    #img1{
+        position: absolute;
+        top: 25%;
+        left: 5%;
+        width: 30%;
+        z-index: 9;
+    }
+    #img2{
+        position: absolute;
+        top: 35%;
+        left: 20%;
+        width: 25%;
+        z-index: 8;
+    }
+
+    #img3{
+        position: absolute;
+        top: 50%;
+        left: 35%;
+        width: 30%;
+        z-index: 9;
+    }
+
+    #img4{
+        position: absolute;
+        top: 30%;
+        left: 33%;
+        width: 30%;
+       
+        z-index: 6;
+    }
+
+    #img5{
+        position: absolute;
+        top: 30%;
+        left: 55%;
+        width: 30%;
+       
+        z-index: 7;
+    }
+    #img6{
+        position: absolute;
+        top: 45%;
+        left: 55%;
+        width: 25%;
+        z-index: 6;
+    }
+    #img7{
+        position: absolute;
+        top:50%;
+        left:80%;
+        width: 25%;
+        z-index: 5;
+    }
+    #img8{
+        position: absolute;
+        top: 45%;
+        right: 10%;
+        width: 25%;
+        height: 250px;
+        z-index: 1;
+    }
+    #img9{
+        position: absolute;
+        top: 20%;
+        right: 0%;
+        width: 25%;
+        z-index: 6;
+    }
     }
 
  `
